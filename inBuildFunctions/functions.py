30334aa52ab74c173e9bea6ee25e46e5ad0b05a3
Ani-4095 <158203163+Ani-4095@users.noreply.github.com>
2024-07-20 08:18:18 +0000

    Changes

---
+++ inBuildFunctions/functions.py
@@ -0,0 +1,13 @@
+str1 = "Dr. Puneeth Raj Kumar"
+
+#To find the Number of characters in the String.
+lengthh = len(str1)
+print("Number of characters in the string are", lengthh)
+
+#To covert all the characters to Uppercase
+print(str1.upper())
+
+#To covert all the characters to Lowercase
+print(str1.lower())
+
+#To replace the something with in a String
